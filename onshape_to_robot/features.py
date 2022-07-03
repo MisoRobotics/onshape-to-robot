7a661f64e83dfe0ee91c830eb36d128caa0cab03
Ryan Sinnet <rsinnet@misorobotics.com>
2022-07-03 21:53:20 +0000

    Fix missing joint limits
    Fix an issue where the feature was being looked up on the wrong assembly
    and no limits were being found. To solve this problem, I added the new
    Onshape Python OpenAPI-based client and used that to retrieve the
    appropriate feature information.

--- onshape_to_robot/features.py
+++ onshape_to_robot/features.py
@@ -1,109 +1,155 @@
+import dataclasses
 import math
-from colorama import Fore, Back, Style
+from typing import (
+    Final,
+    List,
+    Optional,
+    Tuple,
+)
 
-joint_features = {}
+from colorama import Back, Fore, Style
+from onshape_client import Client as OnshapeClient
+from onshape_client import OnshapeElement
+
+from .onshape_api.client import Client
+
 configuration_parameters = {}
 
 
 def init(client, config, root, workspaceId, assemblyId):
-    global configuration_parameters, joint_features
-
-    # Load joint features to get limits later
-    if config['versionId'] == '':
-        joint_features = client.get_features(
-            config['documentId'], workspaceId, assemblyId)
-    else:
-        joint_features = client.get_features(
-            config['documentId'], config['versionId'], assemblyId, type='v')
+    global configuration_parameters
 
     # Retrieving root configuration parameters
     configuration_parameters = {}
-    parts = root['fullConfiguration'].split(';')
+    parts = root["fullConfiguration"].split(";")
     for part in parts:
-        kv = part.split('=')
+        kv = part.split("=")
         if len(kv) == 2:
-            configuration_parameters[kv[0]] = kv[1].replace('+', ' ')
+            configuration_parameters[kv[0]] = kv[1].replace("+", " ")
 
 
 def readExpression(expression):
     # Expression can itself be a variable from configuration
     # XXX: This doesn't handle all expression, only values and variables
-    if expression[0] == '#':
+    if expression[0] == "#":
         expression = configuration_parameters[expression[1:]]
-    if expression[0:2] == '-#':
-        expression = '-'+configuration_parameters[expression[2:]]
+    if expression[0:2] == "-#":
+        expression = "-" + configuration_parameters[expression[2:]]
 
-    parts = expression.split(' ')
+    parts = expression.split(" ")
 
     # Checking the unit, returning only radians and meters
-    if parts[1] == 'deg':
+    if parts[1] == "deg":
         return math.radians(float(parts[0]))
-    elif parts[1] in ['radian', 'rad']:
+    elif parts[1] in ["radian", "rad"]:
         return float(parts[0])
-    elif parts[1] == 'mm':
-        return float(parts[0])/1000.0
-    elif parts[1] == 'm':
+    elif parts[1] == "mm":
+        return float(parts[0]) / 1000.0
+    elif parts[1] == "m":
         return float(parts[0])
-    elif parts[1] == 'in':
-        return float(parts[0])*0.0254
+    elif parts[1] == "in":
+        return float(parts[0]) * 0.0254
     else:
-        print(Fore.RED + 'Unknown unit: '+parts[1] + Style.RESET_ALL)
+        print(Fore.RED + "Unknown unit: " + parts[1] + Style.RESET_ALL)
         exit()
 
 
 def readParameterValue(parameter, name):
     # This is an expression
-    if parameter['typeName'] == 'BTMParameterNullableQuantity':
-        return readExpression(parameter['message']['expression'])
-    if parameter['typeName'] == 'BTMParameterConfigured':
-        message = parameter['message']
-        parameterValue = configuration_parameters[message['configurationParameterId']]
+    if parameter["typeName"] == "BTMParameterNullableQuantity":
+        return readExpression(parameter["message"]["expression"])
+    if parameter["typeName"] == "BTMParameterConfigured":
+        message = parameter["message"]
+        parameterValue = configuration_parameters[message["configurationParameterId"]]
 
-        for value in message['values']:
-            if value['typeName'] == 'BTMConfiguredValueByBoolean':
-                booleanValue = (parameterValue == 'true')
-                if value['message']['booleanValue'] == booleanValue:
-                    return readExpression(value['message']['value']['message']['expression'])
-            elif value['typeName'] == 'BTMConfiguredValueByEnum':
-                if value['message']['enumValue'] == parameterValue:
-                    return readExpression(value['message']['value']['message']['expression'])
+        for value in message["values"]:
+            if value["typeName"] == "BTMConfiguredValueByBoolean":
+                booleanValue = parameterValue == "true"
+                if value["message"]["booleanValue"] == booleanValue:
+                    return readExpression(
+                        value["message"]["value"]["message"]["expression"]
+                    )
+            elif value["typeName"] == "BTMConfiguredValueByEnum":
+                if value["message"]["enumValue"] == parameterValue:
+                    return readExpression(
+                        value["message"]["value"]["message"]["expression"]
+                    )
             else:
-                print(Fore.RED+"Can't read value of parameter "+name+" configured with "+value['typeName']+Style.RESET_ALL)
+                print(
+                    Fore.RED
+                    + "Can't read value of parameter "
+                    + name
+                    + " configured with "
+                    + value["typeName"]
+                    + Style.RESET_ALL
+                )
                 exit()
 
-        print(Fore.RED+"Could not find the value for "+name+Style.RESET_ALL)
+        print(Fore.RED + "Could not find the value for " + name + Style.RESET_ALL)
     else:
-        print(Fore.RED+'Unknown feature type for '+name+': ' +
-              parameter['typeName']+Style.RESET_ALL)
+        print(
+            Fore.RED
+            + "Unknown feature type for "
+            + name
+            + ": "
+            + parameter["typeName"]
+            + Style.RESET_ALL
+        )
         exit()
+
 
 # Gets the limits of a given joint
 
 
-def getLimits(jointType, name):
-    enabled = False
-    minimum, maximum = 0, 0
-    for feature in joint_features['features']:
-        # Find coresponding joint
-        if name == feature['message']['name']:
-            # Find min and max values
-            for parameter in feature['message']['parameters']:
-                if parameter['message']['parameterId'] == "limitsEnabled":
-                    enabled = parameter['message']['value']
+@dataclasses.dataclass
+class FeatureSource:
+    """Fetch feature data from the Onshape API."""
 
-                if jointType == 'revolute':
-                    if parameter['message']['parameterId'] == 'limitAxialZMin':
-                        minimum = readParameterValue(parameter, name)
-                    if parameter['message']['parameterId'] == 'limitAxialZMax':
-                        maximum = readParameterValue(parameter, name)
-                elif jointType == 'prismatic':
-                    if parameter['message']['parameterId'] == 'limitZMin':
-                        minimum = readParameterValue(parameter, name)
-                    if parameter['message']['parameterId'] == 'limitZMax':
-                        maximum = readParameterValue(parameter, name)
-    if enabled:
-        return (minimum, maximum)
-    else:
-        print(Fore.YELLOW + 'WARNING: joint ' + name + ' of type ' +
-              jointType + ' has no limits ' + Style.RESET_ALL)
-        return None
+    def __init__(self, client: OnshapeClient, element: OnshapeElement) -> None:
+        self._client: Final[OnshapeClient] = client
+        self._element: Final[OnshapeElement] = element
+
+    def get_feature(
+        self, name: str, has_parameters: Optional[List[str]] = None
+    ) -> dict:
+        features = self._client.assemblies_api.get_features(
+            self._element.did, self._element.wvm, self._element.wvmid, self._element.eid
+        )
+        for feature in features["features"]:
+            if feature["name"] == name:
+                if has_parameters is None:
+                    return feature
+                if all(
+                    any(key == p.parameter_id for p in feature["parameters"])
+                    for key in has_parameters
+                ):
+                    return feature
+        raise Exception(f"Feature '{name}' not found.")
+
+    def get_limits(self, name: str) -> Optional[Tuple[float, float]]:
+        """Return the joint limits from the named joint on the specified assembly."""
+        feature = self.get_feature(name, ["mateType"])
+        parameters = {p.parameter_id: p for p in feature.parameters}
+        mate_type = parameters["mateType"].value
+        enabled = parameters["limitsEnabled"].value
+        if mate_type == "REVOLUTE":
+            joint_type = "revolute"
+            infix = "Axial"
+        elif mate_type == "SLIDER":
+            joint_type = "prismatic"
+            infix = ""
+        else:
+            raise ValueError(f"Unknown joint type '{joint_type}'")
+
+        if not enabled:
+            print(
+                f"{Fore.YELLOW}WARNING: Joint '{name}' of type '{joint_type}' has no limits.{Style.RESET_ALL}"
+            )
+            return None
+
+        return tuple(
+            (
+                readExpression(parameters[f"limit{infix}Z{suffix}"].expression)
+                for suffix in ("Min", "Max")
+            )
+        )
